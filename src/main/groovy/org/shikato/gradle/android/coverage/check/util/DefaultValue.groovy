de3cf608bf12d2a2e8e7ddb390f2cc3c860ef09a
shikato <nnilan2@gmail.com>
2016-02-07 23:46:24 +0900

    DefaultValueTest

--- src/main/groovy/org/shikato/gradle/android/coverage/check/util/DefaultValue.groovy
+++ src/main/groovy/org/shikato/gradle/android/coverage/check/util/DefaultValue.groovy
@@ -1,7 +1,7 @@
 package org.shikato.gradle.android.coverage.check.util
 
 class DefaultValue {
-    public static final INT = 0;
-    public static final FLOAT = 0F;
-    public static final STRING = "";
+    public static final int INT = 0;
+    public static final float FLOAT = 0F;
+    public static final String STRING = "";
 }
